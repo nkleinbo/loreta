222cf65814a3d407c990eb14f415b25970a5df5b
Ubuntu <ubuntu@anlysis-of-tdna-insertions.openstack.bielefeld>
2019-10-02 15:59:13 +0000

    nothing special

--- analyse_insertion.py
+++ analyse_insertion.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Wed Sep 11 14:45:04 2019
@@ -9,61 +9,58 @@ Created on Wed Sep 11 14:45:04 2019
 import getopt, sys
 import os
 
-fullCmdArguments = sys.argv
-argumentList = fullCmdArguments[1:]
+#move to seperate module later:
+#wether to be verbose or not:
+BE_VERBOSE = True;
+#no of CPUs to use at max:
+NO_CPUS = 64;
+#BLAST parameters:
+BLAST_PARAMS = " -perc_identity 90"
 
-unixOptions = "hf:t:o:b:a:l:"
-gnuOptions = ["help", "fastq=", "tdna=", "output=", "blast=", "assembly=", "line="]
+#runs BLAST and returns an ID file with all hits
+def run_blast(fastqfile, blastfile, tdnafile):
+    #check if makeblastdb has been run
+    if not (os.path.isfile(tdnafile+".nin")):
+        os.system("makeblastdb -dbtype nucl -in "+tdnafile)
+    #run blast if results do not already exist
+    if not (os.path.isfile(blastfile)):
+        if BE_VERBOSE: print ("BLASTING "+fastqfile+" ...")
+        #blastcommand = "gzip -dc "+fastqfile+" | seqtk seq -A | blastn -db "+tdnafile+" -out "+blastfile+" -outfmt 6 -num_threads "+str(NO_CPUS)+" "+BLAST_PARAMS
+        blastcommand = "blastn -query "+fastqfile+" -db "+tdnafile+" -out "+blastfile+" -outfmt 6 -num_threads "+str(NO_CPUS)+" "+BLAST_PARAMS
+        print(blastcommand)
+        os.system(blastcommand)
+        if BE_VERBOSE:  "Done BLASTING "+fastqfile+"."
+    else:
+        if BE_VERBOSE: print ("Skipping BLAST for "+fastqfile+".");
+    if BE_VERBOSE: print ("Creating idfile for "+blastfile+"...")
+    idfile = blastfile+".ids"
+    os.system("cut -f1 "+blastfile+" | sort | uniq > "+idfile)
+    if BE_VERBOSE:  "Done IDFILE "+fastqfile+"."
+    return idfile
 
-try:
-    arguments, values = getopt.getopt(argumentList, unixOptions, gnuOptions)
-except getopt.error as err:
-    print (str(err))
-    sys.exit(2)
-    
-for currentArgument, currentValue in arguments:
-    if currentArgument in ("-h", "--help"):
-        print ("displaying help")
-    elif currentArgument in ("-f", "--fastq"):
-        print (("Location of fastq file: (%s)") % (currentValue))
-        fastqfile = currentValue
-    elif currentArgument in ("-t", "--tdna"):
-        print (("Location of tdnafile: (%s)") % (currentValue))
-        tdnafile = currentValue
-    elif currentArgument in ("-b", "--blast"):
-        print (("Location of blastfile: (%s)") % (currentValue))
-        blastfile = currentValue
-    elif currentArgument in ("-o", "--output"):
-        print (("Results will be stored in: (%s)") % (currentValue))
-        outfile = currentValue
-    elif currentArgument in ("-a", "--assembly"):
-        print (("Assembly results will be stored in: (%s)") % (currentValue))
-        assemblyprefix = currentValue
-    elif currentArgument in ("-l", "--line"):
-        print (("Lineid is: (%s)") % (currentValue))
-        lineid = currentValue
-        
+def filter_sequences(idfile, fastqfile, filtered_fastq):
+    if not (os.path.isfile(filtered_fastq)):
+        if BE_VERBOSE: print ("Grepping sequences from "+fastqfile+"...")
+        os.system("grep -A 3 --no-group-separator -f "+idfile+" "+fastqfile+" > "+filtered_fastq)
+        if BE_VERBOSE: print ("Done grepping sequences from "+fastqfile+".")
+    else:
+        if BE_VERBOSE: print ("Skipping grepping sequences from "+fastqfile+".")
+
+def summarise_results(outdir):
+    print ("Summary "+outdir)
+
+
+def analyse_insertion (line, fastqfile, tdnafile, outdir):
+    #run BLAST
+    blastfile = os.path.join(outdir, line+".bls");
+    idfile = run_blast(fastqfile, blastfile, tdnafile)
+    #filter FASTQ
+    filtered_fastq = os.path.join(outdir, line+".fastq")
+    filter_sequences(idfile, fastqfile, filtered_fastq)
      
 
-if not (os.path.isfile(blastfile)):
-    print "BLASTING..."
-    blastcommand = 'gzip -dc '+fastqfile+' | seqtk seq -A | blastn -db '+tdnafile+ ' -out '+blastfile+' -outfmt 6 -num_threads 4 -perc_identity 90'
-    os.system(blastcommand)
-    print "Done."
-else:
-    print "Skipping BLAST.";
 
-print "Creating idfile..."
-idfile = blastfile+".ids"
-os.system("cut -f1 "+blastfile+" | sort | uniq > "+idfile)
-print "Done."
 
-if not (os.path.isfile(outfile)):
-    print "Grepping sequences..."
-    os.system("zgrep -A 3 --no-group-separator -f "+idfile+" "+fastqfile+" > "+outfile)
-    print "Done."
-else:
-    print "Skipping grepping sequences."
 
 #print "Starting asssembly..."
 #assemblydir=assemblyprefix+"_canu"
