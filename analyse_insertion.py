894395e4675f94ee9f34b9375264e386c88b9f29
Ubuntu <ubuntu@anlysis-of-tdna-insertions.openstack.bielefeld>
2019-10-04 10:16:27 +0000

    nothing special

--- analyse_insertion.py
+++ analyse_insertion.py
@@ -15,7 +15,10 @@ BE_VERBOSE = True;
 #no of CPUs to use at max:
 NO_CPUS = 64;
 #BLAST parameters:
-BLAST_PARAMS = " -perc_identity 90"
+BLAST_PARAMS = " -perc_identity 85"
+#expected genome size:
+GENOME_SIZE=145000000
+
 
 #runs BLAST and returns an ID file with all hits
 def run_blast(fastqfile, blastfile, tdnafile):
@@ -26,7 +29,7 @@ def run_blast(fastqfile, blastfile, tdnafile):
     if not (os.path.isfile(blastfile)):
         if BE_VERBOSE: print ("BLASTING "+fastqfile+" ...")
         #blastcommand = "gzip -dc "+fastqfile+" | seqtk seq -A | blastn -db "+tdnafile+" -out "+blastfile+" -outfmt 6 -num_threads "+str(NO_CPUS)+" "+BLAST_PARAMS
-        blastcommand = "blastn -query "+fastqfile+" -db "+tdnafile+" -out "+blastfile+" -outfmt 6 -num_threads "+str(NO_CPUS)+" "+BLAST_PARAMS
+        blastcommand = "seqtk seq -A "+fastqfile+" | blastn -db "+tdnafile+" -out "+blastfile+" -outfmt 6 -num_threads "+str(NO_CPUS)+" "+BLAST_PARAMS
         print(blastcommand)
         os.system(blastcommand)
         if BE_VERBOSE:  "Done BLASTING "+fastqfile+"."
@@ -35,8 +38,20 @@ def run_blast(fastqfile, blastfile, tdnafile):
     if BE_VERBOSE: print ("Creating idfile for "+blastfile+"...")
     idfile = blastfile+".ids"
     os.system("cut -f1 "+blastfile+" | sort | uniq > "+idfile)
-    if BE_VERBOSE:  "Done IDFILE "+fastqfile+"."
+    if BE_VERBOSE: print("Done IDFILE "+fastqfile+".")
     return idfile
+
+def run_canu(filtered_fastq, filtered_fastq_stats, outdir):
+    assemblydir = os.path.join(outdir, "assembly")
+    projectname = "assembly"
+    contigfile = os.path.join(assemblydir, projectname+".contigs.fasta")
+    if BE_VERBOSE: print ("Starting asssembly for "+filtered_fastq+"...")
+    
+#assemblydir=assemblyprefix+"_canu"
+#canu_command="canu -d "+assemblydir+" -p "+lineid+" -nanopore-raw "+outfile+ " 'genomeSize=100k' 'useGrid=1' 'gridEngineThreadsOption=-pe multislot THREADS' 'gridEngineMemoryOption=-l m_mem_total=MEMORY' 'gridEngineSubmitCommand=qsub -P denbi -l idle=1'"
+#os.system(canu_command)
+#print "Done."
+
 
 def filter_sequences(idfile, fastqfile, filtered_fastq):
     if not (os.path.isfile(filtered_fastq)):
@@ -45,29 +60,78 @@ def filter_sequences(idfile, fastqfile, filtered_fastq):
         if BE_VERBOSE: print ("Done grepping sequences from "+fastqfile+".")
     else:
         if BE_VERBOSE: print ("Skipping grepping sequences from "+fastqfile+".")
+    os.system("wc -l "+fastqfile)
+
+def get_fastq_stats(fastqfile):
+    stats = {}
+    number_reads = 0
+    longest_read = 0
+    total_bases = 0
+    with open(fastqfile, 'r') as fh:
+        i = 0
+        for line in fh:
+            i += 1
+            if(i % 4 == 0):
+                number_reads += 1
+            elif(i % 2 == 0):
+                bases = len(line)
+                total_bases += bases
+                if(bases > longest_read):
+                    longest_read = bases
+    mean_length = total_bases / number_reads
+    coverage_for_longest_read = total_bases / longest_read
+    coverage_for_genome = total_bases / GENOME_SIZE
+    stats["number_reads"] = number_reads
+    stats["longest_read"] = longest_read
+    stats["total_bases"] = total_bases
+    stats["mean_length"] = mean_length
+    stats["coverage_for_longest_read"] = coverage_for_longest_read
+    stats["coverage_for_genome"] = coverage_for_genome
+    return stats
+    
 
 def summarise_results(outdir):
     print ("Summary "+outdir)
 
+def html_summary(line, statistics, webdir):
+    if not (os.path.isdir(webdir)):
+        os.mkdir(webdir)    
+    index_html = os.path.join(webdir, "index.html")
+    fh = open(index_html, "w+")
+    fh.write("<h1>Results for "+line+"</h1>")
+    for head in statistics:
+        fh.write("<h2>"+head+"</h2>")
+        stats = statistics[head]
+        fh.write("<table>")
+        for subhead in stats:
+            fh.write("<tr>")
+            fh.write("<td><b>"+subhead+"</b></td>")
+            fh.write("<td>"+str(stats[subhead])+"</b></td>")
+            fh.write("</tr>")
+        fh.write("</table>")
+    fh.close()
+    
+    return index_html
+    
 
-def analyse_insertion (line, fastqfile, tdnafile, outdir):
+
+def analyse_insertion (line, fastqfile, tdnafile, outdir, webdir):
+    if not (os.path.isdir(outdir)):
+        os.mkdir(outdir)
+    statistics= {}
+    #get statistics for all reads
+    fastq_stats = get_fastq_stats(fastqfile)
+    statistics["fastq_stats"] = fastq_stats
     #run BLAST
     blastfile = os.path.join(outdir, line+".bls");
     idfile = run_blast(fastqfile, blastfile, tdnafile)
     #filter FASTQ
     filtered_fastq = os.path.join(outdir, line+".fastq")
     filter_sequences(idfile, fastqfile, filtered_fastq)
+    filtered_fastq_stats = get_fastq_stats(filtered_fastq)
+    statistics["filtered_fastq_stats"] = filtered_fastq_stats
+    
+    html_summary(line, statistics, webdir)
      
-
-
-
-
-#print "Starting asssembly..."
-#assemblydir=assemblyprefix+"_canu"
-#canu_command="canu -d "+assemblydir+" -p "+lineid+" -nanopore-raw "+outfile+ " 'genomeSize=100k' 'useGrid=1' 'gridEngineThreadsOption=-pe multislot THREADS' 'gridEngineMemoryOption=-l m_mem_total=MEMORY' 'gridEngineSubmitCommand=qsub -P denbi -l idle=1'"
-#os.system(canu_command)
-#print "Done."
-
-
 
 #qaccver saccver pident length mismatch gapopen qstart qend sstart send evalue bitscore
---
+++ run_all.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  2 10:17:35 2019
+
+@author: nkleinbo
+"""
+
+import getopt, sys
+import os
+import analyse_insertion as ai
+import _thread
+
+fullCmdArguments = sys.argv
+argumentList = fullCmdArguments[1:]
+
+unixOptions = "hf:t:o:w:"
+gnuOptions = ["help", "fastqpath=", "tdnafile=", "outputpath=", "webpath="]
+
+try:
+    arguments, values = getopt.getopt(argumentList, unixOptions, gnuOptions)
+except getopt.error as err:
+    print (str(err))
+    sys.exit(2)
+    
+for currentArgument, currentValue in arguments:
+    if currentArgument in ("-h", "--help"):
+        print ("displaying help")
+    elif currentArgument in ("-f", "--fastqpath"):
+        print (("Location of fastq files: (%s)") % (currentValue))
+        fastqpath = currentValue
+    elif currentArgument in ("-t", "--tdna"):
+        print (("Location of tdnafile: (%s)") % (currentValue))
+        tdnafile = currentValue
+    elif currentArgument in ("-o", "--outputpath"):
+        print (("Results will be stored in: (%s)") % (currentValue))
+        outputpath = currentValue
+    elif currentArgument in ("-w", "--webpath"):
+        print (("Webpages will be stored in: (%s)") % (currentValue))
+        webpath = currentValue
+        
+        
+fastq_dict = {}
+for f in os.listdir(fastqpath):
+    prefix, ext = os.path.splitext(f)
+    fastq_dict[prefix] = os.path.join(fastqpath, f)
+
+out_dict = {}
+for line in fastq_dict:
+    outdir = os.path.join(outputpath, line)
+    out_dict[line] = outdir
+
+web_dict = {}
+for line in fastq_dict:
+    webdir = os.path.join(webpath, line)
+    web_dict[line] = webdir
+    
+#print (fastq_dict)
+#print(out_dict)
+
+#run analysis for each line:
+for line in fastq_dict:
+    #_thread.start_new_thread(ai.analyse_insertion(line, fastq_dict[line], tdnafile, out_dict[line]))
+    ai.analyse_insertion(line, fastq_dict[line], tdnafile, out_dict[line], web_dict[line])
+    #break
+
+#for line in fastq_dict:
+#    ai.summarise_results(out_dict[line])
